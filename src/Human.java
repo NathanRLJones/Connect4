f05d0c11d1240f21cc650531cb2b5b2148dda7b5
Nathan Jones <NathanRaymondJones@gmail.com>
2015-05-05 16:45:18 +1000

    Further implementation of Human.java

--- src/Human.java
+++ src/Human.java
@@ -2,23 +2,35 @@ import java.awt.Color;
 
 public class Human implements Player {
 
-	public Human() {
+	private Color color;
+	private String name;
+	
+	public Human(String humanName, Color tokenColor) {
 		// TODO stub
+		name = humanName;
+		color = tokenColor;
 	}
 	
 	public Move getMove() {
 		// TODO stub
-		return null;
+		
+		int column = 0;
+		
+		/*
+		 * Wait for GUI to trigger a MoveChosenEvent
+		 * which should contain a column
+		 * 
+		 */
+		
+		return new Move(column, new Token(this));
 	}
 	
 	public String getName() {
-		// TODO stub
-		return null;
+		return name;
 	}
 	
 	public Color getColor() {
-		// TODO stub
-		return null;
+		return color;
 	}
 	
 }
