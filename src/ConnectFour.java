00ef7aa78445b1d4156496ea9a5e98bb37f65779
Min Jee Son <minjee9108@hotmail.com>
2015-05-08 13:06:54 +1000

    added isGameOver() function - TODO: need to check for diagonal line wins

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -26,17 +26,73 @@ public class ConnectFour {
 	}
 	
 	public void doMove(Move move) {
-		// TODO stub
+		Token token = move.getToken();
+		int column = move.getColumn();
+		
+		board.placeToken(column, token);
 	}
 	
 	public boolean isGameOver() {
 		int height = board.getHeight();
 		int width = board.getWidth();
+		Player possibleWinner = null;
+		int noOfTokens = 0;
+		Player currPlayer;
+		Token currToken;
 		
-		for(int i = 0; i < height; i++){
-			
-		}
-		return true;
+		// Check for a vertical line of 4 same-colour tokens
+		columnloop:
+			for(int i = 0; i < width; i++){
+				rowloop:
+					for(int j = 0; j < height; j++){
+						if(noOfTokens == 4) return true;
+						if(height-j+noOfTokens < 4) continue columnloop;
+						
+						currToken = board.getToken(i, j);
+						if(currToken == null) continue rowloop;
+						currPlayer = currToken.getOwner();
+						if(currPlayer == possibleWinner){
+							noOfTokens++;
+						}else{
+							possibleWinner = currPlayer;
+							noOfTokens = 1;
+						}
+					}
+			}
+		
+		noOfTokens = 0;
+		possibleWinner = null;
+		
+		//Check for a horizontal line of 4 same-colour tokens
+		rowloop:
+			for(int j = 0; j < height; j++){
+				columnloop:
+					for(int i = 0; i < width; i++){
+						if(noOfTokens == 4) return true;
+						if(width-i+noOfTokens < 4) continue rowloop;
+						
+						currToken = board.getToken(i, j);
+						if (currToken == null){
+							possibleWinner = null;
+							noOfTokens = 0;
+							continue columnloop;
+						}
+						currPlayer = currToken.getOwner();
+						if(currPlayer == possibleWinner){
+							noOfTokens++;
+						}else{
+							possibleWinner = currPlayer;
+							noOfTokens = 1;
+						}
+					}
+			}
+		
+		noOfTokens = 0;
+		possibleWinner = null;
+		
+		// TODO check for diagonal lines
+
+		return false;
 	}
 	
 	public Player whoseTurnIsIt() {
