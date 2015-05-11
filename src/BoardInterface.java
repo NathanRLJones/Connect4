f9c204b4d5c3fbbdca62e251783ccca9b7c8fe80
Nathan Jones <NathanRaymondJones@gmail.com>
2015-05-11 19:45:01 +1000

    Added BoardInterface.java

---
+++ src/BoardInterface.java
@@ -0,0 +1,8 @@
+public interface BoardInterface {
+	
+	public int getHeight();
+	public int getWidth();
+	
+	public boolean isSpaceTaken(int column, int row);
+	public boolean isPlayersToken(Player player, int column, int row);
+}
