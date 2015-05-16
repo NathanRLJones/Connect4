290fb0578244cec132a21e75f8a80174612ee8a0
Alen Bou-Haidar <alencool@gmail.com>
2015-05-16 18:18:42 +1000

    Added getColor proxy to token

--- src/Token.java
+++ src/Token.java
@@ -1,9 +1,15 @@
+import java.awt.Color;
+
 public class Token {
 
 	private Player owner;
 	
 	public Token(Player owner) {
 		this.owner = owner;
+	}
+
+	public Color getColor() {
+		return owner.getColor();
 	}
 	
 	public Player getOwner() {
