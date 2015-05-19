d0cdfc781378bc9d10a9d65968e627c5fda21d54
Nicholas Yuwono <alpha.x877@gmail.com>
2015-05-19 16:47:26 +1000

    Added action to the button. Button now does something when pressed

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ButtonPanel extends JPanel {
+public class ButtonPanel extends JPanel implements ActionListener {
 
     private JButton button;
 
@@ -17,15 +19,30 @@ public class ButtonPanel extends JPanel {
         bottomPanel.setOpaque(true);
         bottomPanel.setBackground(Color.WHITE);
 
-        button = new JButton("UNDO");
-        button.setActionCommand("Undo");
+        JButton undoButton = new JButton("UNDO");
+        undoButton.addActionListener(this);
+        undoButton.setActionCommand("Undo");
+
+        JButton redoButton = new JButton("REDO");
+        redoButton.addActionListener(this);
+        redoButton.setActionCommand("Redo");
 
         add(bottomPanel);
-        add(button);
+        add(undoButton);
+        add(redoButton);
     }
 
     public Dimension getPreferredSize() {
         return new Dimension(500,50);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("Undo")) {
+            System.out.println("Button pressed!");
+        } else if (action.equals("Redo")) {
+
+        }
     }
 
 }
