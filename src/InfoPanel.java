e9fe9095363d7bcd6453dc1f30d345d4fc69f572
Lawrence Ling <z5018371@student.unsw.edu.au>
2015-05-19 14:28:25 +1000

    Status label now displays both turn status and game over status in the same label (also larger font)

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -1,30 +1,43 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class InfoPanel extends JPanel {
 
-	private JLabel turnLabel;
+	private JLabel statusLabel;
 	
+	// TODO make it scale
 	public InfoPanel() {
         super();
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setOpaque(true);
         setBackground(Color.WHITE);
         
-        turnLabel = new JLabel();
-        add(turnLabel, BorderLayout.NORTH);
+        add(Box.createVerticalGlue());
+        
+        JPanel topPanel = new JPanel();
+        topPanel.setOpaque(true);
+        topPanel.setBackground(Color.WHITE);
+        
+        statusLabel = new JLabel();
+        statusLabel.setFont(new Font("Arial", Font.PLAIN, 32));
+        topPanel.add(statusLabel);
+        
+        add(topPanel);
     }
 
 	public Dimension getPreferredSize() {
         return new Dimension(500,50);
     }
 	
-	public void setTurnLabel(String text) {
-		turnLabel.setText(text);
+	public void setStatusLabel(String text) {
+		statusLabel.setText(text);
 	}
 	
 }
