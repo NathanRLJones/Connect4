1026393ebd65df9cb6dbc4b5ac0bf3a359a2b97e
Nicholas Yuwono <alpha.x877@gmail.com>
2015-05-25 22:43:30 +1000

    Changed button position

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -15,27 +15,37 @@ public class ButtonPanel extends JPanel implements ActionListener {
         setOpaque(true);
         setBackground(Color.WHITE);
 
-        add(Box.createVerticalGlue());
+        //add(Box.createVerticalGlue());
 
         JPanel bottomPanel = new JPanel();
         bottomPanel.setOpaque(true);
-        bottomPanel.setBackground(Color.WHITE);
+        bottomPanel.setBackground(Color.BLACK);
+        //bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        bottomPanel.setLayout(new GridBagLayout());
 
-        JButton undoButton = new JButton("UNDO");
+        GridBagConstraints c = new GridBagConstraints();
+
+        JButton undoButton = new JButton("UNDO");           //Adding undo button
         undoButton.addActionListener(this);
         undoButton.setActionCommand("Undo");
+        c.insets = new Insets(10,10,0,10);                  //Button extra space(top, left, bottom, right)
 
         JButton redoButton = new JButton("REDO");
         redoButton.addActionListener(this);
         redoButton.setActionCommand("Redo");
+        c.insets = new Insets(10,10,0,10);
 
+        bottomPanel.add(undoButton, c);
+        bottomPanel.add(redoButton, c);
+        //bottomPanel.add(undoButton);
+        //bottomPanel.add(redoButton);
         add(bottomPanel);
-        add(undoButton);
-        add(redoButton);
+        //add(undoButton);
+        //add(redoButton);
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(500,50);
+        return new Dimension(100,60);
     }
 
     public void actionPerformed(ActionEvent e) {
