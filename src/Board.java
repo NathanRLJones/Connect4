eb060e4b7d989c21a1e201971c7067b92ff3c23f
Nicholas Yuwono <alpha.x877@gmail.com>
2015-05-29 19:46:01 +1000

    Added javadoc comment

--- src/Board.java
+++ src/Board.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Board.
+ * Creates empty board on the screen
  */
 public class Board implements BoardInterface{
 	
-	/** The columns. */
+	/** The list of columns. */
 	private ArrayList<ArrayList<Token>> columns;
 	
-	/** The height. */
+	/** The height of the board. */
 	private int height;
 	
-	/** The width. */
+	/** The width of the board. */
 	private int width;
 
 	/**
